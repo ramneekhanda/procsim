4a7744b4255d69bb59461db3d4c68b3762047ef0
Ramneek Handa <ramneekhanda@gmail.com>
2024-08-25 23:47:35 +0800

    some changes

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -3,16 +3,28 @@
     import Canvas from "./Canvas.svelte";
     import { Pane, Splitpanes } from "svelte-splitpanes";
     import Menubar from "$lib/components/menubar/menubar.svelte";
-    import { compile_code } from './dsa.js';
+ import init, { compile_code, get_code_schema } from './dsa.js';
+ import { onMount } from "svelte";
 
     let codeEditor;
+ let schema = "";
 
+ onMount(async () => {
+     init().catch((error) => {
+         if (!error.message.startsWith("Using exceptions for control flow, don't mind me. This isn't actually an error!")) {
+             console.log("actual error location is here");
+             throw error;
+         }
+     }).then(() => {
+         schema = get_code_schema();
+         console.log("got schema!");
+     });
+ });
     function compileCode() {
         let b = compile_code(codeEditor.getCode());
-        console.log(b.errorLog);
+        console.log(b.error_log);
     }
 </script>
-
 <div class="flex flex-col h-full">
     <div class="flex-initial">
         <Menubar on:runClicked={() => compileCode()}/>
@@ -24,8 +36,9 @@
         </Pane>
         <Pane>
             <div class="flex flex-col h-full">
-                <Monaco bind:this={codeEditor}/>
+                <Monaco bind:this={codeEditor} schema = {schema}/>
             </div>
         </Pane>
     </Splitpanes>
 </div>
+
