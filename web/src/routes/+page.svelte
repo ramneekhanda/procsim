df6a26e52cb190c18d15b82a156b22a0dfc5f822
Ramneek Handa <ramneekhanda@gmail.com>
2024-09-04 10:12:24 +0000

    minor changes

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,15 +1,155 @@
 <script lang="ts">
   import Monaco from "./Monaco.svelte";
   import Canvas from "./Canvas.svelte";
-  import { Pane, Splitpanes } from "svelte-splitpanes";
   import Menubar from "$lib/components/menubar/menubar.svelte";
   import init, { compile_code, get_code_schema } from "./dsa";
   import { onMount } from "svelte";
+  import "dockview-core/dist/styles/dockview.css";
+  import { createDockview } from "dockview-core";
+  import "dockview-core/dist/styles/dockview.css";
+  import Grid from "gridjs-svelte";
+  import "gridjs/dist/theme/mermaid.css";
+  import type {
+    GroupPanelPartInitParameters,
+    IContentRenderer,
+    IDockviewPanel,
+    ITabRenderer,
+  } from "dockview-core";
 
-  let codeEditor;
+  let codeEditor: Monaco;
   let schema = "";
+  let dockView: HTMLElement;
+  $: codeEditor && codeEditor.$set({ schema });
+  
+  const data = [
+    { name: "John", email: "john@example.com" },
+    { name: "Mark", email: "mark@gmail.com" },
+  ]
+  class MonacoPanel implements IContentRenderer {
+    private readonly _element: HTMLElement;
+
+    get element(): HTMLElement {
+      return this._element;
+    }
+
+    constructor() {
+      const MonacoDiv = document.createElement("div");
+      MonacoDiv.style.width = "100%";
+      MonacoDiv.style.height = "100%";
+      let monaco = new Monaco({ target: MonacoDiv, props: { schema } });
+      codeEditor = monaco;
+      this._element = MonacoDiv;
+    }
+
+    init(parameters: GroupPanelPartInitParameters): void {}
+  }
+
+  class LogPanel implements IContentRenderer {
+    private readonly _element: HTMLElement;
+
+    get element(): HTMLElement {
+      return this._element;
+    }
+
+    constructor() {
+      const logsDiv = document.createElement("div");
+      let monaco = new Grid({ target: logsDiv, props: { data } });
+      this._element = logsDiv;
+    }
+
+    init(parameters: GroupPanelPartInitParameters): void {}
+  }
+
+  class Tab implements ITabRenderer {
+    private _element: HTMLElement;
+    private _title: string;
+
+    get element(): HTMLElement {
+      return this._element;
+    }
+
+    constructor() {
+      this._element = document.createElement("div");
+      this._title = "new tab";
+    }
+
+    init(parameters: GroupPanelPartInitParameters): void {
+      var text = document.createTextNode(parameters.title);
+      this._element.appendChild(text);
+    }
+  }
+
+  class ViewPanel implements IContentRenderer {
+    private readonly _element: HTMLElement;
+
+    get element(): HTMLElement {
+      return this._element;
+    }
+
+    constructor() {
+      const CanvasDiv = document.createElement("div");
+      CanvasDiv.style.width = "100%";
+      CanvasDiv.style.height = "100%";
+      let canvas = new Canvas({ target: CanvasDiv });
+      this._element = CanvasDiv;
+    }
+
+    init(parameters: GroupPanelPartInitParameters): void {}
+  }
 
   onMount(async () => {
+    const api = createDockview(dockView, {
+      className: "dockview-theme-replit",
+      createComponent: (options) => {
+        switch (options.name) {
+          case "MonacoPanel":
+            return new MonacoPanel();
+          case "ViewPanel":
+            return new ViewPanel();
+          case "LogPanel":
+            return new LogPanel();
+          default:
+            throw new Error(`Unknown component ${options.name}`);
+        }
+      },
+      createTabComponent: (options) => {
+        switch (options.name) {
+          case "Tab":
+            return new Tab();
+          default:
+            throw new Error(`Unknown tab component ${options.name}`);
+        }
+      },
+    });
+
+    api.addPanel({
+      id: "view_panel",
+      component: "ViewPanel",
+      tabComponent: "Tab",
+      title: "View",
+    });
+
+    const monacoPanel: IDockviewPanel = api.addPanel({
+      id: "monaco_panel",
+      component: "MonacoPanel",
+      title: "Editor",
+      tabComponent: "Tab",
+      position: {
+        direction: "right",
+      },
+    });
+
+    api.addPanel({
+      id: "log_panel",
+      component: "LogPanel",
+      title: "Logs",
+      tabComponent: "Tab",
+      position: {
+        referencePanel: monacoPanel,
+        direction: "below",
+      },
+    });
+
     init()
       .catch((error) => {
         if (
@@ -23,9 +163,9 @@
       })
       .then(() => {
         schema = get_code_schema();
-        console.log("got schema!");
       });
   });
+
   function compileCode() {
     let b = compile_code(codeEditor.getCode());
     console.log(b.error_log);
@@ -34,12 +174,5 @@
 
 <div class="flex">
   <Menubar on:runClicked={() => compileCode()} />
-  <Splitpanes horizontal={false}>
-    <Pane >
-      <Canvas/>
-    </Pane>
-    <Pane>
-      <Monaco bind:this={codeEditor} {schema} />
-    </Pane>
-  </Splitpanes>
+  <div class="flex" bind:this={dockView}></div>
 </div>
--- web/src/routes/Monaco.svelte
+++ web/src/routes/Monaco.svelte
@@ -1,88 +1,84 @@
 <script lang="ts">
-    import "../app.css";
-    import type monaco from "monaco-editor";
+  import "../app.css";
+  import type monaco from "monaco-editor";
 
- import { configureMonacoYaml } from 'monaco-yaml'
+  import { configureMonacoYaml } from "monaco-yaml";
 
-    import { onMount } from "svelte";
+  import { onMount } from "svelte";
 
- import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
- import YamlWorker from "./monaco_yaml.worker.js?worker";
+  import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+  import YamlWorker from "./monaco_yaml.worker.js?worker";
 
- let Monaco;
- export let schema = "";
-    let editorElement: HTMLDivElement;
-    let editor: monaco.editor.IStandaloneCodeEditor;
+  let Monaco: typeof monaco;
+  export let schema = "";
+  let editorElement: HTMLDivElement;
+  let editor: monaco.editor.IStandaloneCodeEditor;
 
- function configureMonaco() {
-     if (typeof Monaco !== "undefined" && schema !== "") {
-         console.log ("configured monaco")
-         console.log(JSON.parse(schema))
-
-         configureMonacoYaml(Monaco, {
-             enableSchemaRequest: true,
-             completion: true,
-             schemas: [
-                 {
-                     fileMatch: ['*.yaml'],
-                     schema: JSON.parse(schema),
-                     uri: "http://google.com/"
-                 }
-             ]
-         });
-     }
- }
- $: if (schema !== '') {
-     configureMonaco();
- }
-
-    export let value: string = "";
-
-    export function getCode(){
-        return editor.getValue()
+  function configureMonaco() {
+    if (typeof Monaco !== "undefined" && schema !== "") {
+      configureMonacoYaml(Monaco, {
+        enableSchemaRequest: true,
+        completion: true,
+        schemas: [
+          {
+            fileMatch: ["*.yaml"],
+            schema: JSON.parse(schema),
+            uri: "http://google.com/",
+          },
+        ],
+      });
     }
+  }
+  $: if (schema !== "") {
+    configureMonaco();
+  }
 
- onMount(async() => {
-     // @ts-ignore
-     window.MonacoEnvironment = {
-            getWorker: function (_moduleId: any, label: string) {
-             switch (label) {
-                 case 'editorWorkerService':
-                     return new EditorWorker();
-                 case 'yaml':
-                     let worker = new YamlWorker();
-                     return worker;
-                 default:
-                     throw new Error(`Unknown label ${label}`)
-             }
-            },
-        };
+  export let value: string = "";
 
-        Monaco = await import("monaco-editor");
+  export function getCode() {
+    return editor.getValue();
+  }
 
-        editor = Monaco.editor.create(editorElement, {
-            minimap: { enabled: false },
-            automaticLayout: true,
-            scrollBeyondLastLine: false,
-         quickSuggestions: {
-             other: true,
-             comments: false,
-             strings: true
-         }
-     });
-     console.log ("on mounted monaco")
-     configureMonaco();
-     let yamlModel = Monaco.editor.createModel(
-         value,
-         "yaml",
-         Monaco.Uri.parse('inmemory://mymodel.yaml')
-     )
-     editor.setModel(yamlModel);
+  onMount(async () => {
+    // @ts-ignore
+    window.MonacoEnvironment = {
+      getWorker: function (_moduleId: any, label: string) {
+        switch (label) {
+          case "editorWorkerService":
+            return new EditorWorker();
+          case "yaml":
+            let worker = new YamlWorker();
+            return worker;
+          default:
+            throw new Error(`Unknown label ${label}`);
+        }
+      },
+    };
 
-        return () => {
-            editor.dispose();
-        };
+    Monaco = await import("monaco-editor");
+
+    editor = Monaco.editor.create(editorElement, {
+      minimap: { enabled: false },
+      automaticLayout: true,
+      scrollBeyondLastLine: false,
+      quickSuggestions: {
+        other: true,
+        comments: false,
+        strings: true,
+      },
     });
+    configureMonaco();
+    let yamlModel = Monaco.editor.createModel(
+      value,
+      "yaml",
+      Monaco.Uri.parse("inmemory://mymodel.yaml"),
+    );
+    editor.setModel(yamlModel);
+
+    return () => {
+      editor.dispose();
+    };
+  });
 </script>
 
 <div class="monaco-container flex h-fit" bind:this={editorElement} />
